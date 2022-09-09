3d1e4e29e45c71812c61933f5ab75afb1fce6861
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-08 20:17:58 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", Okay 59);
+	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", Okay 59);
 	return (1);
 }
