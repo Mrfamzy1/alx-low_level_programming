8d8e6df87b25ee605fe250b0e86df0d5925e54d2
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-08 20:06:19 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
-
-/**
- *
- * main - program entry point 
- *
- * Return: 1 if success
- */
-int main(void)
-{
-	printf(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	return (1); 
-}
-
-#include <stdio.h>
 
 int main(void)
 {
