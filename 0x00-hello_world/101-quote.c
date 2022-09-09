740b61680e569f575bddbbb83e0a8734f0e74fed
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-08 18:13:46 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
+
 /**
-*main-program entry point.
+* main - print the string in the put function
 *
+* Description: using the main funcation 
+* this program prints "exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
 *Return: 1 on success.
 */
 int main(void)
 {
-	write (2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	printf(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
