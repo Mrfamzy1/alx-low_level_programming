1105e7f72b8821516c4f95ea6e1628817122e7da
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-08 19:29:11 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-/**
- * 
- * main - program entry point
- *
- * Return: 1 (Success)
- */
-int main(void)
-{
-	write (2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+/** 
+* main - program entry point                          
+
+ *                                                     
+
+ * Return: 1 (Success)                                 
+
+ */                                                    
+
+int main(void)                                         
+
+{                                                      
+
+        write (2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59); 
+
+        return (1);                                    
+
