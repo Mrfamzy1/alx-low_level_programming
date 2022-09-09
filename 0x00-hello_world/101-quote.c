ce84bebc15208f2bca8426bd2860334bacc3ff55
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-08 20:04:29 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *
@@ -10,4 +11,17 @@ int main(void)
 {
 	printf(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1); 
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 5;
+	int b = 4;
+
+	for (a < b;;)
+	{
+		    printf("%d\n", a++);
+	}
 }
