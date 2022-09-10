f04886c42240679974dac8fbb497a6e10ef5330d
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-09 20:47:12 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,49 +1,34 @@
 #include <stdio.h>
 
 
-
 /**
-
 * main - entry point
-
 * Return: always 0 (success)
-
 */
 
 int main(void)
-
 {
 
-int n;
+	int n;
+	n = 48;
+		while (n <= 57) /57 corresponds with ASCII character 9/
+	{
+			putchar (n);
+		if (n != 57)
+	{
 
+	putchar(',');
 
+	putchar(' ');
 
-n = 48;
+	}	
 
+		n++;
 
+	}
 
-while (n <= 57) /57 corresponds with ASCII character 9/
+		putchar ('\n'); /add new line/
 
-{
-
-putchar (n);
-
-if (n != 57)
-
-{
-
-putchar(',');
-
-putchar(' ');
-
-}
-
-n++;
-
-}
-
-putchar ('\n'); /add new line/
-
-return (0);
+	return (0);
 
 }
