a764aca0b5ceba02435616847aff5bb78f262c7e
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-10 02:59:01 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Return: always 0 (success)
+ * main - Prints all possible combinations of single-digit numbers.
+ * Return: 0 if successful, non-zero otherwise
+ * Return: Always 0.
  */
-
 int main(void)
+
 {
-int n;
-n = 48;
-while (n <= 57) /**57 corresponds with ASCII character 9*/
+int num = 0;
+int num;
+while (1)
+for (num = 0; num <= 9; num++)
 {
-putchar (n);
-if (n != 57)
+putchar((num % 10) + '0');
+if (num == 9)
 {
+break;
+}
+else
+{
+putchar(44);
+putchar(32);
+}
+num++;
+continue;
 putchar(',');
 putchar(' ');
 }
-n++;
-}
-putchar ('\n'); /**add new line*/
+putchar('\n');
 return (0);
 }
