3e9e810229f4e028e7384f1ee5623c6beec1a8c7
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-10 04:25:20 -0700

    Task 8

--- 0x01-variables_if_else_while/101-quote.c
+++ 0x01-variables_if_else_while/101-quote.c
@@ -2,12 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
  * Return: 1
  */
 
 int main(void)
 {
-printf(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
