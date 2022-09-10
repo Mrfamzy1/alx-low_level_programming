c502731ef7c45201b0222d8dc654e037151739fa
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-10 04:08:24 -0700

    Task 8

--- 0x01-variables_if_else_while/101-quote.c
+++ 0x01-variables_if_else_while/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,11 +8,6 @@
 
 int main(void)
 {
-int a = 5;
-int b = 4;
-
-for (a < b;;)
-{
-printf("%d\n", a++);
-}
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
