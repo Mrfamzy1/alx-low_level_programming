54363e60071388b9e8b37531bc3448b3b3f3f45b
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-10 02:36:29 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,14 +13,16 @@ while (n <= 57)
 /**57 corresponds with ASCII character 9*/
 {
 putchar (n);
+}
 if (n != 57)
 {
 putchar(',');
 putchar(' ');
 }
 n++;
-}
+{
 putchar ('\n');
+}
 /**add new line*/
 return (0);
 }
