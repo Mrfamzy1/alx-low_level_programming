0eec82bf60bf9a084df57cc0382e25054272284f
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-13 14:47:43 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point 
+ *
+ * Description: program that prints _putchar, followed by a new line
+ * Return: 0;
+ */
+void print_alphabet(void)
+{
+	int j;
+	for (j = 'a'; j <= 'z'; j++)	
+	{	
+	_putchar(j);
+	}
+	_putchar('\n');
+}
