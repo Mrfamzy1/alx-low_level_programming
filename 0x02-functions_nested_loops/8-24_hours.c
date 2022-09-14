91bda224b9031fcf6c20ba6d207c401d78bf35d7
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-13 21:30:09 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,8 @@
 /**
  *
  * jack_bauer - unction that prints every minute 
- * of the day of Jack Bauer, starting from 00:00 to 23:59
- *
+ * n of the day of Jack Bauer, starting from 00:00 to 23:59
+ * and resets mins 
  * Return: 0
  */
 
@@ -16,7 +16,7 @@ void jack_bauer(void)
 	int hours_remainder;
 	int mins_remainder;
 
-	while (hours <= 59)
+	while (hours <= 23)
 	{
 	while minutes <= 59)
 	{
