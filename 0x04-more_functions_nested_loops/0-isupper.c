bd98c6d83fec5b6d7fcc87ccfbefde5afdf3a1cb
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-15 08:28:03 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet - Entry point
- *
  * Return: Always 0 (Success)
  */
 
@@ -10,7 +9,7 @@ void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 'A'; i <= 'Z'; i++)
 	{
 		_putchar(i);
 	}
