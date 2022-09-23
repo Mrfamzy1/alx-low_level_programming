3ad321fc81a53542bfdab0b2c0f55ebcda08d446
Mrfamzy1 <james.afamozoagu@gmail.com>
2022-09-23 04:52:30 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,8 +11,8 @@ p = &n;
 /**
 * write your line of code here...
 * Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
+* @a - you are not allowed to use a
+* @p - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
